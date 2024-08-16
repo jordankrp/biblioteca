5b1d5803bbefe70ffd839f6113181cd634fa79f2
Jordan Karapanagiotis <jordan.krp.93@gmail.com>
2024-08-16 16:13:54 +0200

    setup poetry

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ An API for interacting with a database of books that are added and rated by user
 - To retrieve all books with their average rating, no user login is needed.
 - To create, update, delete or rate a book, user login must be done first.
 - A user can rate his or someone else's book only once. They can update their rating after.
+
+## Instructions
+Run `conda env create -f environment.yml` to create a conda environment and install all dependencies with
--- app/database.py
+++ app/database.py
@@ -5,9 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import psycopg2
-from psycopg2.extras import RealDictCursor
-
 from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
@@ -23,20 +20,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-# while True:
-#     try:
-#         conn = psycopg2.connect(
-#             host="localhost",
-#             database="bibliotecadb",
-#             user="postgres",
-#             password=PASSWORD,
-#             cursor_factory=RealDictCursor
-#         )
-#         cursor = conn.cursor()
-#         print("Database connection was successfull")
-#         break
-#     except Exception as error:
-#         print("Connection to DB failed")
-#         print(f"Error: {error}")
-#         time.sleep(2)
